f0cb05cdabcbd2c8774731374f76a2e2c5335815
Vasil Atanasov <vsatanasov@gmail.com>
2016-02-09 22:49:20 +0200

    Own autoload fix

--- Autoloader.php
+++ Autoloader.php
@@ -11,7 +11,7 @@ namespace HttpStub;
 
 class Autoloader
 {
-    const BASE_NAMESPACE_NAME = 'HttpStubOld';
+    const BASE_NAMESPACE_NAME = 'HttpStub';
 
     public static function load($className)
     {
